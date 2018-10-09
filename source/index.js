2d995968d50fd23281e2035283ae95948ead9769
Dima Vakatsiienko <d.vakatsiienko@lectrum.io>
2018-10-09 18:12:44 +0300

    next

--- source/index.js
+++ source/index.js
@@ -5,6 +5,6 @@ import ReactDOM from 'react-dom';
 // Theme
 import './theme/init';
 
-const start = <div className = 'start'>Добро пожаловать!</div>;
+const start = <div className = 'start'>Поехали!</div>;
 
 ReactDOM.render(start, document.getElementById('app'));
