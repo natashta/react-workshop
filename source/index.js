dcd761326445791bfe311598f1d0767696bea62e
Dima Vakatsiienko <d.vakatsiienko@lectrum.io>
2018-10-09 20:25:21 +0300

    → Checkpoint at: 21:23:59

--- source/index.js
+++ source/index.js
@@ -5,6 +5,6 @@ import ReactDOM from 'react-dom';
 // Theme
 import './theme/init';
 
-const start = <div className = 'start'>Поехали!</div>;
+const start = <div className = 'start'>Андрюха Привет!</div>;
 
 ReactDOM.render(start, document.getElementById('app'));
