f08252884a41e384bfcbcfa686bd02224512bf33
Dima Vakatsiienko <d.vakatsiienko@lectrum.io>
2018-10-16 20:06:42 +0300

    → Checkpoint at: 20:39:46

--- source/components/Profile.js
+++ source/components/Profile.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import avatar from '../theme/assets/avatars/nasa.jpg';
 
 export default class Profile extends Component {
     render() {
         return (
             <div className = 'profile'>
-
+                <img
+                    className = 'avatar'
+                    src = { avatar }
+                />
             </div>
         );
     }
