59f80aeadafdc45a266614754a2f9b6f7bd89369
Dima Vakatsiienko <d.vakatsiienko@lectrum.io>
2018-10-16 20:06:42 +0300

    → Checkpoint at: 20:24:16

--- source/pages/Instagram.js
+++ source/pages/Instagram.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 
 export default class Instagram extends Component {
     render() {
-        return <div className = 'start'>Поехали!!!</div>;
+        return (
+            <>
+                <div>Header</div>
+            </>
+        );
     }
 }
